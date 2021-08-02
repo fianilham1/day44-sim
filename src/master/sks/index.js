449f016160f335b2d7b29cfccf9daeb12caf52bd
Alfaizuna <78782379+Alfaizuna@users.noreply.github.com>
2021-08-02 14:02:53 +0700

    list sks ke body (#7)
    Co-authored-by: Muhammad Adzka A <adzka123@gmail.com>

--- src/master/sks/index.js
+++ src/master/sks/index.js
@@ -6,26 +6,27 @@ class MasterSKS extends Component {
     constructor(props) {
         super(props);
         this.state = { 
-            listSks :[ {idsks: 1, matkul:"Basic Java", jmlsks:"4", idjurusan: "IT", dosen:""},
-                       {idsks: 2, matkul:"Basic PHP", jmlsks:"4", idjurusan: "IT", dosen:""},
-                       {idsks: 3, matkul:"Basis Data", jmlsks:"3", idjurusan: "IT", dosen:""},
-                       {idsks: 4, matkul:"Ilmu Nutrisi Ternak Dasar", jmlsks:"4", idjurusan: "PETERNAKAN", dosen:""},
-                       {idsks: 5, matkul:"Anatomi Ternak", jmlsks:"3", idjurusan: "PETERNAKAN", dosen:""},
-                       {idsks: 6, matkul:"Ilmu Ternak Potong", jmlsks:"4", idjurusan: "PETERNAKAN", dosen:""},
-                       {idsks: 7, matkul:"Biologi Seluler", jmlsks:"4", idjurusan: "KEDOKTERAN", dosen:""},
-                       {idsks: 8, matkul:"Genetika", jmlsks:"4", idjurusan: "KEDOKTERAN", dosen:""},
-                       {idsks: 9, matkul:"Ilmu Bedah", jmlsks:"3", idjurusan: "KEDOKTERAN", dosen:""} ]
+            // listSks :[ {idsks: 1, matkul:"Basic Java", jmlsks:"4", idjurusan: "IT", dosen:""},
+            //            {idsks: 2, matkul:"Basic PHP", jmlsks:"4", idjurusan: "IT", dosen:""},
+            //            {idsks: 3, matkul:"Basis Data", jmlsks:"3", idjurusan: "IT", dosen:""},
+            //            {idsks: 4, matkul:"Ilmu Nutrisi Ternak Dasar", jmlsks:"4", idjurusan: "PETERNAKAN", dosen:""},
+            //            {idsks: 5, matkul:"Anatomi Ternak", jmlsks:"3", idjurusan: "PETERNAKAN", dosen:""},
+            //            {idsks: 6, matkul:"Ilmu Ternak Potong", jmlsks:"4", idjurusan: "PETERNAKAN", dosen:""},
+            //            {idsks: 7, matkul:"Biologi Seluler", jmlsks:"4", idjurusan: "KEDOKTERAN", dosen:""},
+            //            {idsks: 8, matkul:"Genetika", jmlsks:"4", idjurusan: "KEDOKTERAN", dosen:""},
+            //            {idsks: 9, matkul:"Ilmu Bedah", jmlsks:"3", idjurusan: "KEDOKTERAN", dosen:""} ]
          }
     }
 
-    editlist = newData =>{
-        this.setState({
-            listSks:newData
-        })
-    }
+    // editlist = newData =>{
+    //     this.setState({
+    //         listSks:newData
+    //     })
+    // }
 
     renderedMaster= () =>{
-        return <TabelSks listSks={this.state.listSks} editlist={this.editlist}/>
+        const {editlist, listSks} = this.props
+        return <TabelSks listSks={listSks} editlist={editlist}/>
     }
 
     render() { 
--- src/template/body/index.js
+++ src/template/body/index.js
@@ -9,6 +9,15 @@ class Body extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            listSks :[ {idsks: 1, matkul:"Basic Java", jmlsks:"4", idjurusan: "IT", dosen:""},
+                       {idsks: 2, matkul:"Basic PHP", jmlsks:"4", idjurusan: "IT", dosen:""},
+                       {idsks: 3, matkul:"Basis Data", jmlsks:"3", idjurusan: "IT", dosen:""},
+                       {idsks: 4, matkul:"Ilmu Nutrisi Ternak Dasar", jmlsks:"4", idjurusan: "PETERNAKAN", dosen:""},
+                       {idsks: 5, matkul:"Anatomi Ternak", jmlsks:"3", idjurusan: "PETERNAKAN", dosen:""},
+                       {idsks: 6, matkul:"Ilmu Ternak Potong", jmlsks:"4", idjurusan: "PETERNAKAN", dosen:""},
+                       {idsks: 7, matkul:"Biologi Seluler", jmlsks:"4", idjurusan: "KEDOKTERAN", dosen:""},
+                       {idsks: 8, matkul:"Genetika", jmlsks:"4", idjurusan: "KEDOKTERAN", dosen:""},
+                       {idsks: 9, matkul:"Ilmu Bedah", jmlsks:"3", idjurusan: "KEDOKTERAN", dosen:""} ],
             users: [],
             mhsEdit: {},
             listPenerimaan: [
@@ -138,6 +147,12 @@ class Body extends Component {
         })
     }
 
+    editlist = newData =>{
+        this.setState({
+            listSks:newData
+        })
+    }
+
     renderPage = () => {
         console.log("PROFILE SELECTED",this.state.mhsProfileDetail)
         const {currentPage, goToPage} = this.props
@@ -180,7 +195,7 @@ class Body extends Component {
             return <DetailProfile mhsProfileDetail={this.state.mhsProfileDetail} goToPage={goToPage} saveProfile={this.saveProfileHandler}/>
 
         if (currentPage === "sks")
-            return <ListSKS />
+            return <ListSKS listSks={this.state.listSks} editlist={this.editlist}/>
 
         return ""
 
